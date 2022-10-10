616d06e5652dfb61f5b7117fd2d08b1200587b4b
Kjayndom <107386723+Kjayndom@users.noreply.github.com>
2022-10-10 16:08:39 +0100

    Update and rename index.js to caesar cipher converter.js

---
+++ caesar cipher converter.js
@@ -0,0 +1,20 @@
+****CAESARS CIPHER****
+
+function CIPHER(str) {
+	const letters = 
+	["A", "B", "C", "D", "E", "F", "G", "H","I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+	"A", "B", "C", "D", "E", "F", "G", "H","I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+	
+	let cipherText = "";
+
+	for (let i=0; i < str.length; i++){
+		if (letters.includes(str[i])){
+			let shift = letters.indexOf(str[i]) + 13
+			cipherText += letters[shift]
+		}else {cipherText += str[i]}
+	}
+ 	return cipherText;
+}
+
+console.log(CIPHER("SERR PBQR PNZC"));
+
